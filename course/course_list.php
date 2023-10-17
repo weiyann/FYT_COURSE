316b117b953e09c32e29e6944a7ab62da409846d
weiyann <jona11105@gmail.com>
2023-10-17 23:41:17 +0800

    拆解頁面

---
+++ course/course_list.php
@@ -0,0 +1,37 @@
+<?php include './parts/html-head.php' ?>
+<?php include './parts/sidebar.php' ?>
+<?php include './parts/topbar.php' ?>
+
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+  <!-- Page Heading -->
+  <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+  <div class="container my-auto">
+    <div class="copyright text-center my-auto">
+      <span>Copyright &copy; Your Website 2020</span>
+    </div>
+  </div>
+</footer>
+<!-- End of Footer -->
+
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<?php include './parts/scripts.php' ?>
+
+
+<?php include './parts/html-foot.php' ?>
---
+++ course/parts/sidebar.php
@@ -0,0 +1,102 @@
+ <!-- Page Wrapper -->
+ <div id="wrapper">
+
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="../index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">FreeFyt</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="../index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        唯一五人組
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+            aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>會員中心</span>
+        </a>                
+    </li>
+
+    <!-- Nav Item - Utilities Collapse Menu：健康平台 -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <i class="fas fa-fw fa-wrench"></i>
+            <span>健康平台</span>
+        </a>
+    </li>
+    <!-- Nav Item - Pages Collapse Menu：課程專區 -->
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseCourse"
+          aria-expanded="true" aria-controls="collapsePages">
+          <i class="fa-solid fa-dumbbell"></i>
+          <span>課程專區</span>
+      </a>
+          <div id="collapseCourse" class="collapse" aria-labelledby="headingPages"
+              data-parent="#accordionSidebar">
+              <div class="bg-white py-2 collapse-inner rounded">
+                  <a class="collapse-item" href="./course/course_management.html">課程管理系統</a>
+                  <a class="collapse-item" href="">報名資料管理系統</a>
+              </div>
+          </div>
+      
+  </li>
+
+    <!-- Nav Item - Pages Collapse Menu：購物商城 -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseStore"
+            aria-expanded="true" aria-controls="collapsePages">
+            <i class="fas fa-store"></i>
+            <span>購物商城</span>
+        </a>
+        <div id="collapseStore" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="product_management.html">商品管理系統</a>
+                <a class="collapse-item" href="">訂單管理系統</a>
+                </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Pages Collapse Menu： 部落論壇 -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+            aria-expanded="true" aria-controls="collapsePages">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>部落論壇</span>
+        </a>
+    </li>
+
+
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
+<!-- End of Sidebar -->
