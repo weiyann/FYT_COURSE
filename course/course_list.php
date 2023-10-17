4ea0d152e894266cf9d71ae2deac54441e6be371
weiyann <jona11105@gmail.com>
2023-10-18 01:53:04 +0800

    上傳資料表(未完成)

--- course/course_list.php
+++ course/course_list.php
@@ -3,7 +3,40 @@ require './parts/connect_db.php';
 $pageName = 'course_list';
 $title = '課程管理列表';
 
-$sql = "SELECT * FROM course ORDER BY course_id DESC LIMIT 0, 20";
+/*
+SELECT 
+  c.course_id, 
+  c.course_name, 
+  c.course_description, 
+  m.member_name, 
+  c.creation_date, 
+  c.is_published, 
+  ct.day_of_week, 
+  ct.time_period, 
+  cat.category
+FROM course c
+INNER JOIN course_time ct ON c.course_id = ct.course_id
+INNER JOIN course_category_relation ccr ON c.course_id = ccr.course_id
+INNER JOIN category cat ON ccr.category_id = cat.category_id
+INNER JOIN coach co ON c.coach_id = co.coach_id
+INNER JOIN member m ON co.member_id = m.member_id;
+*/
+$sql = "SELECT 
+c.course_id, 
+c.course_name, 
+c.course_description, 
+m.member_name, 
+c.creation_date, 
+c.is_published, 
+ct.day_of_week, 
+ct.time_period, 
+cat.category
+FROM course c
+INNER JOIN course_time ct ON c.course_id = ct.course_id
+INNER JOIN course_category_relation ccr ON c.course_id = ccr.course_id
+INNER JOIN category cat ON ccr.category_id = cat.category_id
+INNER JOIN coach co ON c.coach_id = co.coach_id
+INNER JOIN member m ON co.member_id = m.member_id ORDER BY c.course_id";
 $rows = $pdo->query($sql)->fetchAll();
 ?>
 
@@ -15,9 +48,40 @@ $rows = $pdo->query($sql)->fetchAll();
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
-  <pre><?php
-  print_r($rows);
-  ?></pre>
+<div class="row">
+    <div class="col">
+      <table class="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">課程名稱</th>
+            <th scope="col">課程描述</th>
+            <th scope="col">教練姓名</th>
+            <th scope="col">日期</th>
+            <th scope="col">上架狀態</th>
+            <th scope="col">星期</th>
+            <th scope="col">時間</th>
+            <th scope="col">分類</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach($rows as $r): ?>
+          <tr>
+            <td><?= $r['course_id'] ?></td>
+            <td><?= $r['course_name'] ?></td>
+            <td><?= $r['course_description'] ?></td>
+            <td><?= $r['member_name'] ?></td>
+            <td><?= $r['creation_date'] ?></td>
+            <td><?= $r['is_published'] ?></td>
+            <td><?= $r['day_of_week'] ?></td>
+            <td><?= $r['time_period'] ?></td>
+            <td><?= $r['category'] ?></td>
+          </tr>
+          <?php endforeach ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
 
 </div>
 <!-- /.container-fluid -->
