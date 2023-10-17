1f9ae116e6f8ac982e315c76329817ae7cb1cdb0
weiyann <jona11105@gmail.com>
2023-10-17 23:59:24 +0800

    列表和新增的頁面

--- course/parts/html-head.php
+++ course/parts/html-head.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>訂單管理 - FreeFyt</title>
+    <title>課程管理 - FreeFyt</title>
 
     <!-- Custom fonts for this template-->
     <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
