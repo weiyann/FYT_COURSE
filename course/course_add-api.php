37cd0aef95d9c7ecb3fb185e31217d3f1568ad5c
weiyann <jona11105@gmail.com>
2023-10-18 11:04:18 +0800

    新增的表單和api

--- course/course_add-api.php
+++ course/course_add-api.php
@@ -1,2 +1,19 @@
 <?php
 echo json_encode($_POST);
+$sql = "INSERT INTO `course`(
+  `course_name`, `email`, `mobile`, `birthday`, `address`, `created_at`
+  ) VALUES (
+    ?, ?, ?, ?, ?, NOW()
+  )";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([
+  $_POST['name'],
+  $_POST['email'],
+  $_POST['mobile'],
+  $_POST['birthday'],
+  $_POST['address'],
+]);
+echo json_encode([
+  'postData' => $_POST,
+  'rowCount' => $stmt->rowCount(),
+]);
