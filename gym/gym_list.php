f9c9495a8d1a53a69665058670d3916304ae0d87
weiyann <jona11105@gmail.com>
2023-10-22 10:12:43 +0800

    add pagination

--- gym/gym_list.php
+++ gym/gym_list.php
@@ -3,11 +3,37 @@ require './parts/connect_db.php';
 $pageName = 'gym_list';
 $title = '健身房管理列表';
 
-$sql = 'SELECT * FROM gym JOIN district on gym.district_id = district.district_id
- ORDER BY gym_id desc';
-$rows=$pdo ->query($sql)->fetchAll();
+$perPage = 10;
 
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+if ($page < 1) {
+  header('Location: ?page=1'); #頁面轉向
+  exit; # 直接結束這支 php
+}
 
+$t_sql = "SELECT COUNT(1) FROM gym";
+# 總筆數
+$totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
+
+# 預設值
+$totalPages = 0;
+$rows = [];
+
+// 有資料時
+if ($totalRows > 0) {
+  # 總頁數
+  $totalPages = ceil($totalRows / $perPage); # 無條件進位，不足20筆也算一頁
+  if ($page > $totalPages) {
+    header('Location: ?page=' . $totalPages); #頁面轉向最後一頁
+    exit;
+  }
+}
+$sql = sprintf('SELECT * FROM gym JOIN district on gym.district_id = district.district_id
+ ORDER BY gym_id desc
+ limit %s,%s',
+ ($page - 1) * $perPage,
+$perPage);
+$rows = $pdo->query($sql)->fetchAll();
 ?>
 
 <?php include './parts/html-head.php' ?>
@@ -17,8 +43,34 @@ $rows=$pdo ->query($sql)->fetchAll();
 
 <!-- Begin Page Content -->
 <div class="container-fluid">
-
-<div class="row">
+  <div class="row">
+    <div class="col">
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item <?= $page == 1 ? 'disabled' : '' ?>">
+            <a class="page-link" href="?page=1">
+              <i class="fa-solid fa-angles-left"></i>
+            </a>
+          </li>
+          <?php for ($i = $page - 3; $i <= $page + 3; $i++):
+            if ($i >= 1 and $i <= $totalPages): ?>
+              <li class="page-item <?= $i == $page ? 'active' : '' ?>">
+                <a class="page-link" href="?page=<?= $i ?>">
+                  <?= $i ?>
+                </a>
+              </li>
+              <?php
+            endif;
+          endfor; ?>
+          <li class="page-item <?= $page == $totalPages ? 'disabled' : '' ?>">
+            <a class="page-link" href="?page=<?= $totalPages ?>">
+              <i class="fa-solid fa-angles-right"></i></a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  <div class="row">
     <div class="col">
       <table class="table table-bordered table-striped">
         <thead>
@@ -60,7 +112,7 @@ $rows=$pdo ->query($sql)->fetchAll();
                 <?= $r['business_time'] ?>
               </td>
               <td>
-                <?= $r['district_name'] .$r['gym_address'] ?>
+                <?= $r['district_name'] . $r['gym_address'] ?>
               </td>
               <td>
                 <?= $r['created_at'] ?>
